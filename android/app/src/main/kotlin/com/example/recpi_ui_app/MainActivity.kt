8a50e3d555768ba2b368552ed8ee136c163f065b
AbedUllahAboSamra <abodayman2002@gmail.com>
2022-10-19 02:29:42 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/recpi_ui_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recpi_ui_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/profile/prrofile_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../components/my_bottom_nav_bar.dart';
+import '../../constants.dart';
+import '../../size_config.dart';
+import '../home/components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor,
+      leading: SizedBox(),
+      // On Android it's false by default
+      centerTitle: true,
+      title: Text("Profile"),
+      actions: <Widget>[
+        MaterialButton(
+          onPressed: () {},
+          child: Text(
+            "Edit",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: SizeConfig.defaultSize * 1.6, //16
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
